62e3d6efdc7961906c9a0e143feaa995aa13fa08
Artyom Mikhailov <happytoad42@gmail.com>
2020-10-21 11:22:46 +0700

    added successContext and tests

---
+++ src/contexts/successContext.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow, moount, mount } from 'enzyme';
+
+import successContext from './successContext';
+
+// a functional component that call useSuccess for test purpose
+const FunctionalComponent = () => {
+  successContext.useSuccess();
+  return <div></div>;
+};
+
+test('useSuccess throws error when not wrapper in SuccessProvider', () => {
+  expect(() => {
+    shallow(<FunctionalComponent />);
+  }).toThrow('useSuccess must be used within SuccessProvider');
+});
+
+test('useSuccess does not throw error while wrapper in SuccessProvider', () => {
+  expect(() => {
+    mount(
+      <successContext.SuccessProvider>
+        <FunctionalComponent />
+      </successContext.SuccessProvider>
+    );
+  }).not.toThrow();
+});
